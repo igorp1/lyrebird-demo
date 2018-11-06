7c8a4728ca06d098099ea3b49fcff8b965070047
idp <igordepaula1@gmail.com>
2018-11-06 21:48:11 -0200

    cleaned up env vairiables

--- env.js
+++ env.js
@@ -3,15 +3,13 @@ let env = {}
 switch(process.env.NODE_ENV) {
     case 'production':
         env = {
-            ENV_NAME : 'prod', 
-            API_BASE : ''
+            ENV_NAME : 'prod'
         }
         break
     case 'dev':
     default:
         env = {
-            ENV_NAME : 'dev',
-            API_BASE : 'http://localhost:8000'
+            ENV_NAME : 'dev'
         }
 }
 
