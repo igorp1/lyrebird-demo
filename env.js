65f69f67d3471e7140ecdb005df0e64088fa6229
idp <igordepaula1@gmail.com>
2018-11-08 14:18:55 -0200

    updated env variables

--- env.js
+++ env.js
@@ -3,13 +3,17 @@ let env = {}
 switch(process.env.NODE_ENV) {
     case 'production':
         env = {
-            ENV_NAME : 'prod'
+            ENV_NAME : 'prod',
+            LYREBIRD_CLIENT_ID : process.env.LYREBIRD_CLIENT
         }
         break
     case 'dev':
     default:
         env = {
-            ENV_NAME : 'dev'
+            ENV_NAME : 'dev',
+            LYREBIRD_CLIENT_ID : process.env.LYREBIRD_CLIENT,
+            LYREBIRD_REDIRECT_URI : 'http://localhost:3000/auth',
+            AUTH_STATE : 'wX3mIWzPJj'
         }
 }
 
