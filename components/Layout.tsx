cce84fa7dccb85e11716b017bd14d94045cb74b1
idp <igordepaula1@gmail.com>
2018-11-08 14:20:15 -0200

    added side menu to layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,8 +1,22 @@
 import styled from 'styled-components'
+import SideMenu from './SideMenu'
+import Router from 'next/router'
+import NProgress from 'nprogress'
 
+
+Router.events.on('routeChangeStart', (_url : string) => { NProgress.start() })
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 const LayoutStyled = styled.div`
-    padding: 2em;
+    display: flex;
+    div.children-container { 
+        padding: 2em; 
+        box-sizing: border-box;
+        width:calc(100vw - 225px);
+        height: 100vh;
+        overflow-y: scroll;
+    }
 `
 
 
@@ -11,7 +25,10 @@ interface LayoutProps {
 }
 const Layout = ({ children } : LayoutProps) => (
     <LayoutStyled>
-        { children }
+        <SideMenu />
+        <div className='children-container'>
+            { children }
+        </div>
     </LayoutStyled>
 )
 
